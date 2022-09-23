8b964840c0fbff6f681aa49f6dff7940be99de5f
Simon Baudart <simon@contact-me.be>
2022-09-23 17:01:26 +0200

    Validate the token

--- src/web/lib/middlewares/withAuth.ts
+++ src/web/lib/middlewares/withAuth.ts
@@ -1,5 +1,6 @@
 ﻿import {NextApiRequest, NextApiResponse} from "next";
-const passport = require('passport');
+import jwksClient from 'jwks-rsa';
+import jwt from 'jsonwebtoken'
 
 export function withAuth(handler: { (req: NextApiRequest, res: NextApiResponse): void; (arg0: NextApiRequest, arg1: NextApiResponse): any; }) {
     return async (req: NextApiRequest, res: NextApiResponse) => {
@@ -8,24 +9,37 @@ export function withAuth(handler: { (req: NextApiRequest, res: NextApiResponse):
             return res.status(401).end('Not authenticated. No Auth header');
         }
 
-        /*const token = authHeader.split(' ')[1];
-        let decodedToken;
-        try {
-            decodedToken = await auth.verifyIdToken(token);
-            if (!decodedToken || !decodedToken.uid)
-                return res.status(401).end('Not authenticated');
-            req.uid = decodedToken.uid;
-        } catch (error) {
-            console.log(error.errorInfo);
-            const errorCode = error.errorInfo.code;
-            error.status = 401;
-            if (errorCode === 'auth/internal-error') {
-                error.status = 500;
-            }
-            //TODO handlle firebase admin errors in more detail
-            return res.status(error.status).json({ error: errorCode });
-        }*/
+        const client = jwksClient({
+            jwksUri: "https://" + process.env.NEXT_PUBLIC_AZURE_AD_TENANT_NAME + ".b2clogin.com/" + process.env.NEXT_PUBLIC_AZURE_AD_TENANT_NAME + ".onmicrosoft.com/" + process.env.NEXT_PUBLIC_AZURE_AD_USER_FLOW + "/discovery/v2.0/keys"
+        });
 
-        return handler(req, res);
+        function getKey(header: any, callback: any){
+            client.getSigningKey(header.kid, function(err, key) {
+                console.log(key);
+                // @ts-ignore
+                const signingKey = key?.publicKey || key?.rsaPublicKey;
+                callback(null, signingKey);
+            });
+        }
+        
+        const options = {
+            audience: process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_ID
+        };
+        
+        const token = req.headers.authorization?.split(' ')[1]!;
+
+        let decodedToken;
+        
+        jwt.verify(token, getKey, options, function(err, decoded) {
+            decodedToken = decoded;
+            console.log(decodedToken);
+
+            if (!decodedToken)
+            {
+                return res.status(401).end('Not authenticated. No Auth header');
+            }
+
+            return handler(req, res);
+        });
     };
 }
