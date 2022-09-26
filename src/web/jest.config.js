37e8d910ad0d8dcf7563a2f952620697bc31e6ea
Simon Baudart <simon@contact-me.be>
2022-09-26 15:59:59 +0200

    Add code coverage report in Sonar

--- src/web/jest.config.js
+++ src/web/jest.config.js
@@ -13,7 +13,9 @@ const customJestConfig = {
     // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
     moduleDirectories: ['node_modules', '<rootDir>/'],
+    "collectCoverage": true,
     testEnvironment: 'jest-environment-jsdom',
+    testResultsProcessor: "jest-sonar-reporter"
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
