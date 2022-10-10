6f64b94d886943629b8f08984dc67159e63d0ead
Simon Baudart <simon@contact-me.be>
2022-10-10 15:20:33 +0200

    Add the mutation to create/update/remove a patient

--- src/api/Prism.ProAssistant.Api/Mutations/PatientMutation.cs
+++ src/api/Prism.ProAssistant.Api/Mutations/PatientMutation.cs
@@ -6,6 +6,7 @@
 
 using MongoDB.Driver;
 using Prism.ProAssistant.Business.Models;
+using Prism.ProAssistant.Business.Security;
 using Prism.ProAssistant.Business.Storage;
 
 namespace Prism.ProAssistant.Api.Mutations;
@@ -15,14 +16,20 @@ public class PatientMutation
 {
     public async Task<Patient> CreatePatientAsync(Patient patient, [Service] IOrganizationContext organizationContext)
     {
+        patient.Id = Identifier.Generate();
         await organizationContext.Patients.InsertOneAsync(patient);
 
         return patient;
     }
+    
+    public async Task<Patient> UpdatePatientAsync(Patient patient, [Service] IOrganizationContext organizationContext)
+    {
+        return await organizationContext.Patients.FindOneAndReplaceAsync(Builders<Patient>.Filter.Eq("Id", patient.Id), patient);
+    }
 
     public async Task<bool> RemovePatientAsync(Guid id, [Service] IOrganizationContext organizationContext)
     {
-        var result = await organizationContext.Patients.DeleteOneAsync(Builders<Patient>.Filter.Eq("id", id));
+        var result = await organizationContext.Patients.DeleteOneAsync(Builders<Patient>.Filter.Eq("Id", id));
 
         return result.IsAcknowledged;
     }
