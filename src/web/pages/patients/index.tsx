27a9890da04e198b687c365bdf2cd959e0ccfd70
Simon Baudart <simon@contact-me.be>
2022-10-10 17:28:07 +0200

    Create a simple design

---
+++ src/web/pages/patients/index.tsx
@@ -0,0 +1,14 @@
+﻿import {NextPage} from "next";
+import useTranslation from "next-translate/useTranslation";
+import ContentContainer from "../../components/design/ContentContainer";
+
+const Patients: NextPage = () => {
+
+    const {t} = useTranslation('patients');
+
+    return <ContentContainer>
+        <h1>{t("title")}</h1>
+    </ContentContainer>
+}
+
+export default Patients;
