da609ec0028c76adc54c23c196bdcdc41f78a45d
Simon Baudart <simon@contact-me.be>
2022-10-21 16:29:13 +0200

    Search patients case insensitive and starts with

--- src/api/Prism.ProAssistant.Api/Graph/Patients/PatientQuery.cs
+++ src/api/Prism.ProAssistant.Api/Graph/Patients/PatientQuery.cs
@@ -4,8 +4,10 @@
 //  </copyright>
 // -----------------------------------------------------------------------
 
+using System.Text.RegularExpressions;
 using HotChocolate.AspNetCore.Authorization;
 using HotChocolate.Data;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using Prism.ProAssistant.Business.Models;
 using Prism.ProAssistant.Business.Storage;
@@ -20,6 +22,40 @@ public class PatientQuery
     public IExecutable<Patient> GetPatientById(Guid id, [Service] IOrganizationContext organizationContext)
     {
         return organizationContext.Patients.Find(x => x.Id == id).AsExecutable();
+    }
+
+    [UseSorting]
+    public IExecutable<Patient> SearchPatients(string lastName, string firstName, string phoneNumber, string birthDate, [Service] IOrganizationContext organizationContext)
+    {
+        var filters = new List<FilterDefinition<Patient>>();
+
+        if (!string.IsNullOrWhiteSpace(lastName))
+        {
+            filters.Add(Builders<Patient>.Filter.Regex(x => x.LastName, BsonRegularExpression.Create(new Regex($"^{lastName}", RegexOptions.IgnoreCase))));
+        }
+
+        if (!string.IsNullOrWhiteSpace(firstName))
+        {
+            filters.Add(Builders<Patient>.Filter.Regex(x => x.FirstName, BsonRegularExpression.Create(new Regex($"^{firstName}", RegexOptions.IgnoreCase))));
+        }
+
+        if (!string.IsNullOrWhiteSpace(phoneNumber))
+        {
+            filters.Add(Builders<Patient>.Filter.Regex(x => x.PhoneNumber, BsonRegularExpression.Create(new Regex($"^{phoneNumber}", RegexOptions.IgnoreCase))));
+        }
+
+        if (!string.IsNullOrWhiteSpace(birthDate))
+        {
+            filters.Add(Builders<Patient>.Filter.Regex(x => x.BirthDate, BsonRegularExpression.Create(new Regex($"^{birthDate}", RegexOptions.IgnoreCase))));
+        }
+
+        var filter = filters.Count == 0
+            ? Builders<Patient>.Filter.Empty
+            : Builders<Patient>.Filter.And(filters);
+
+        return organizationContext.Patients
+            .Find(filter)
+            .AsExecutable();
     }
 
     [UsePaging]
