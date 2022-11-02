0a586cecade56283780fe505da08013aa9e4ba9d
Simon Baudart <simon@contact-me.be>
2022-11-02 11:30:38 +0100

    Add logs with Serilog and ELK

--- src/api/Prism.ProAssistant.Api/Program.cs
+++ src/api/Prism.ProAssistant.Api/Program.cs
@@ -20,8 +20,45 @@ using Prism.ProAssistant.Business;
 using Prism.ProAssistant.Business.Behaviors;
 using Prism.ProAssistant.Business.Security;
 using Prism.ProAssistant.Business.Storage;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Logs
+Serilog.Debugging.SelfLog.Enable(Console.WriteLine);
+var logBuilder = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+    .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {CorrelationId} {Level:u3}] {Message} (at {Caller}){NewLine}{Exception}")
+    .WriteTo.File("./logs/pro-assistant.txt", rollingInterval: RollingInterval.Day, retainedFileCountLimit: 7)
+    .Enrich.FromLogContext()
+    .Enrich.FromLogContext()
+    .Enrich.WithCorrelationId()
+    .Enrich.WithClientIp()
+    .Enrich.WithClientAgent()
+    .Enrich.WithProperty("environment", Environment.GetEnvironmentVariable("ENVIRONMENT") ?? "proassistant");
+
+var elkUri = Environment.GetEnvironmentVariable("ELK_URI");
+if (!string.IsNullOrWhiteSpace(elkUri) && !builder.Environment.IsDevelopment())
+{
+    logBuilder.WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elkUri))
+    {
+        BatchAction = ElasticOpType.Create,
+        TypeName = null,
+        IndexFormat = Environment.GetEnvironmentVariable("ELK_INDEX_FORMAT"),
+        ModifyConnectionSettings = x => x.BasicAuthentication(Environment.GetEnvironmentVariable("ELK_INDEX_AUTH_USER"), Environment.GetEnvironmentVariable("ELK_INDEX_AUTH_PASSWORD")),
+        FailureCallback = e => Console.WriteLine("Unable to submit event " + e.MessageTemplate),
+        EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog |
+                           EmitEventFailureHandling.WriteToFailureSink |
+                           EmitEventFailureHandling.RaiseCallback,
+        InlineFields = true
+    });
+}
+
+Log.Logger = logBuilder.CreateLogger();
+builder.Host.UseSerilog();
 
 // Add Mediatr
 var applicationAssembly = typeof(EntryPoint).Assembly;
