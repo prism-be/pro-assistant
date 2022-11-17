7a561ce03df0c338f1697c241426a2e560918587
Simon Baudart <simon@contact-me.be>
2022-11-17 13:06:38 +0100

    Complete unit tests on Models

--- src/api/Prism.ProAssistant.Business.Tests/SerializationTests.cs
+++ src/api/Prism.ProAssistant.Business.Tests/SerializationTests.cs
@@ -9,96 +9,114 @@ using FluentAssertions;
 using Prism.ProAssistant.Business.Models;
 using Prism.ProAssistant.Business.Security;
 
-namespace Prism.ProAssistant.Business.Tests;
-
-public class SerializationTests
+namespace Prism.ProAssistant.Business.Tests
 {
-    private readonly Random _dice = new();
-
-    [Fact]
-    public void Configuration_Ok()
+    public class SerializationTests
     {
-        // Arrange
-        var source = new Setting
+        private readonly Random _dice = new();
+
+        [Fact]
+        public void Configuration_Ok()
         {
-            Id = Identifier.GenerateString(),
-            Value = Identifier.GenerateString()
-        };
+            // Arrange
+            var source = new Setting
+            {
+                Id = Identifier.GenerateString(),
+                Value = Identifier.GenerateString()
+            };
 
-        // Act and Assert
-        CheckSerialization(source);
-    }
+            // Act and Assert
+            CheckSerialization(source);
+        }
 
-    [Fact]
-    public void Meeting_Ok()
-    {
-        // Arrange
-        var meeting = new Meeting
+        [Fact]
+        public void History_Ok()
         {
-            Id = Identifier.GenerateString(),
-            Duration = _dice.Next(0, 42),
-            Payment = (int)PaymentTypes.Wire,
-            Price = _dice.Next(0, 42),
-            State = (int)MeetingState.Confirmed,
-            PaymentDate = DateTime.UtcNow,
-            Title = Identifier.GenerateString(),
-            PatientId = Identifier.GenerateString(),
-            StartDate = DateTime.UtcNow.AddHours(-2)
-        };
+            // Arrange
+            var source = new History
+            {
+                Id = Identifier.GenerateString(),
+                Actual = new object(),
+                ModificationDate = DateTime.Now,
+                TypeName = Identifier.GenerateString(),
+                UserId = Identifier.GenerateString()
+            };
 
-        // Act and Assert
-        CheckSerialization(meeting);
-    }
+            // Act and Assert
+            CheckSerialization(source);
+        }
 
-    [Fact]
-    public void Patient_Ok()
-    {
-        // Arrange
-        var patient = new Patient
+        [Fact]
+        public void Meeting_Ok()
         {
-            Id = Identifier.GenerateString(),
-            BirthDate = Identifier.GenerateString(),
-            City = Identifier.GenerateString(),
-            Country = Identifier.GenerateString(),
-            Number = Identifier.GenerateString(),
-            Street = Identifier.GenerateString(),
-            FirstName = Identifier.GenerateString(),
-            LastName = Identifier.GenerateString(),
-            ZipCode = Identifier.GenerateString()
-        };
+            // Arrange
+            var meeting = new Meeting
+            {
+                Id = Identifier.GenerateString(),
+                Duration = _dice.Next(0, 42),
+                Payment = (int)PaymentTypes.Wire,
+                Price = _dice.Next(0, 42),
+                State = (int)MeetingState.Confirmed,
+                PaymentDate = DateTime.UtcNow,
+                Title = Identifier.GenerateString(),
+                PatientId = Identifier.GenerateString(),
+                StartDate = DateTime.UtcNow.AddHours(-2)
+            };
 
-        // Act and Assert
-        CheckSerialization(patient);
-    }
+            // Act and Assert
+            CheckSerialization(meeting);
+        }
 
-    [Fact]
-    public void Tariff_Ok()
-    {
-        // Arrange
-        var organization = new Tariff
+        [Fact]
+        public void Patient_Ok()
         {
-            Id = Identifier.GenerateString(),
-            Name = Identifier.GenerateString(),
-            Price = (decimal)_dice.NextDouble(),
-            DefaultDuration = _dice.Next(0, 42)
-        };
+            // Arrange
+            var patient = new Patient
+            {
+                Id = Identifier.GenerateString(),
+                BirthDate = Identifier.GenerateString(),
+                City = Identifier.GenerateString(),
+                Country = Identifier.GenerateString(),
+                Number = Identifier.GenerateString(),
+                Street = Identifier.GenerateString(),
+                FirstName = Identifier.GenerateString(),
+                LastName = Identifier.GenerateString(),
+                ZipCode = Identifier.GenerateString()
+            };
 
-        // Act and Assert
-        CheckSerialization(organization);
-    }
+            // Act and Assert
+            CheckSerialization(patient);
+        }
 
-    private static void CheckSerialization<T>(T source)
-    {
-        // Act
-        var destination = SerializeAndDeserialize(source);
+        [Fact]
+        public void Tariff_Ok()
+        {
+            // Arrange
+            var organization = new Tariff
+            {
+                Id = Identifier.GenerateString(),
+                Name = Identifier.GenerateString(),
+                Price = (decimal)_dice.NextDouble(),
+                DefaultDuration = _dice.Next(0, 42)
+            };
 
-        // Assert
-        destination.Should().BeEquivalentTo(source);
-    }
+            // Act and Assert
+            CheckSerialization(organization);
+        }
 
-    private static T? SerializeAndDeserialize<T>(T source)
-    {
-        var json = JsonSerializer.Serialize(source);
-        return JsonSerializer.Deserialize<T>(json);
+        private static void CheckSerialization<T>(T source)
+        {
+            // Act
+            var destination = SerializeAndDeserialize(source);
+
+            // Assert
+            destination.Should().BeEquivalentTo(source);
+        }
+
+        private static T? SerializeAndDeserialize<T>(T source)
+        {
+            var json = JsonSerializer.Serialize(source);
+            return JsonSerializer.Deserialize<T>(json);
+        }
     }
 }
