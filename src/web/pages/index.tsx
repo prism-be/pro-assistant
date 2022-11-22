06b7632c84b1e35255c68e00f32979bc58f738c8
Simon Baudart <simon@contact-me.be>
2022-11-22 16:34:37 +0100

    Update design to make it more clean

--- src/web/pages/index.tsx
+++ src/web/pages/index.tsx
@@ -1,7 +1,15 @@
 import type {NextPage} from 'next'
 import ContentContainer from "../components/design/ContentContainer";
+import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 const Home: NextPage = () => {
+
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push('/patients');
+    }, []);
 
     return (
         <ContentContainer>
