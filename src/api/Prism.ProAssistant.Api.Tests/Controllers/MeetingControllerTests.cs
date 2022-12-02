f36f4936c957c9c3499120bafa4aacacb59180cd
Simon Baudart <simon@contact-me.be>
2022-12-02 16:47:13 +0100

    Add unit tests to match the case

--- src/api/Prism.ProAssistant.Api.Tests/Controllers/MeetingControllerTests.cs
+++ src/api/Prism.ProAssistant.Api.Tests/Controllers/MeetingControllerTests.cs
@@ -42,6 +42,47 @@ namespace Prism.ProAssistant.Api.Tests.Controllers
             result.Result.Should().BeAssignableTo<OkObjectResult>();
             mediator.Verify(x => x.Send(It.IsAny<SearchMeetings>(), CancellationToken.None), Times.Once);
         }
+        
+        [Fact]
+        public async Task Search_Must_Filter()
+        {
+            // Arrange
+            var mediator = new Mock<IMediator>();
+            mediator.Setup(x => x.Send(It.IsAny<SearchMeetings>(), CancellationToken.None))
+                .ReturnsAsync(new List<Meeting>()
+                {
+                    new Meeting
+                    {
+                        Id = Identifier.GenerateString(),
+                        State = (int)MeetingState.Created
+                    },
+                    new Meeting
+                    {
+                        Id = Identifier.GenerateString(),
+                        State = (int)MeetingState.Confirmed
+                    },
+                    new Meeting
+                    {
+                        Id = Identifier.GenerateString(),
+                        State = (int)MeetingState.Done
+                    },
+                    new Meeting
+                    {
+                        Id = Identifier.GenerateString(),
+                        State = (int)MeetingState.Canceled
+                    }
+                });
+
+            // Act
+            var controller = new MeetingController(mediator.Object);
+            var result = await controller.Search(new SearchMeetings(DateTime.Today, DateTime.Today.AddDays(-7), null));
+
+            // Assert
+            result.Result.Should().BeAssignableTo<OkObjectResult>();
+            mediator.Verify(x => x.Send(It.IsAny<SearchMeetings>(), CancellationToken.None), Times.Once);
+            var items = ((OkObjectResult)result.Result!).Value as List<Meeting>;
+            items!.Count.Should().Be(3);
+        }
 
         [Fact]
         public async Task UpsertOne()
