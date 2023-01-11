a14d77075f925e3c243f2d27fccde138fe2b6da8
Simon Baudart <simon@contact-me.be>
2023-01-11 09:58:30 +0100

    Add healthchecks on the api and the dependencies

---
+++ src/api/Prism.ProAssistant.Api/HealthChecks/CheckCache.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file = "CheckCache.cs" company = "Prism">
+//  Copyright (c) Prism.All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using System.Text;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Prism.ProAssistant.Business.Security;
+
+namespace Prism.ProAssistant.Api.HealthChecks;
+
+public class CheckCache : IHealthCheck
+{
+    private readonly IDistributedCache _cache;
+
+    public CheckCache(IDistributedCache cache)
+    {
+        _cache = cache;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new())
+    {
+        try
+        {
+            var id = Identifier.GenerateString();
+            var data = Encoding.UTF8.GetBytes(id);
+
+            await _cache.SetAsync(id, data, cancellationToken);
+            await _cache.RemoveAsync(id, cancellationToken);
+
+            return HealthCheckResult.Healthy("The cache is healthy.");
+        }
+        catch (Exception ex)
+        {
+            return HealthCheckResult.Unhealthy("The cache is unhealthy.", ex);
+        }
+    }
+}
