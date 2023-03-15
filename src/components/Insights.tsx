243f60c8938b18c3e33f7458a99e049bc535d651
Simon Baudart <simon@contact-me.be>
2023-03-15 16:19:22 +0100

    Add Application Insights on the client side

---
+++ src/components/Insights.tsx
@@ -0,0 +1,27 @@
+﻿import { ApplicationInsights } from "@microsoft/applicationinsights-web";
+import { ReactPlugin, withAITracking, AppInsightsContext } from "@microsoft/applicationinsights-react-js";
+
+let reactPlugin = new ReactPlugin();
+let appInsights = new ApplicationInsights({
+    config: {
+        connectionString: process.env.NEXT_PUBLIC_APPLICATIONINSIGHTS_CONNECTION_STRING,
+        enableAutoRouteTracking: true,
+        enableCorsCorrelation: true,
+        isBrowserLinkTrackingEnabled: true,
+        extensions: [reactPlugin],
+    },
+});
+
+try {
+    appInsights.loadAppInsights();
+} catch (e) {}
+
+interface Props {
+    children: any;
+}
+
+const AzureAppInsights = ({ children }: Props) => {
+    return <AppInsightsContext.Provider value={reactPlugin}>{children}</AppInsightsContext.Provider>;
+};
+
+export default withAITracking(reactPlugin, AzureAppInsights);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,18 +6,21 @@ import Head from "next/head";
 import { Alert } from "@/components/Alert";
 import { getData } from "@/libs/http";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+import Insights from "@/components/Insights";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
     return (
-        <UserProvider>
-            <Head>
-                <title>Pro Assistant</title>
-            </Head>
-            <SWRConfig value={{ fetcher: getData }}>
-                <Component {...pageProps} />
-                <Alert />
-            </SWRConfig>
-        </UserProvider>
+        <Insights>
+            <UserProvider>
+                <Head>
+                    <title>Pro Assistant</title>
+                </Head>
+                <SWRConfig value={{ fetcher: getData }}>
+                    <Component {...pageProps} />
+                    <Alert />
+                </SWRConfig>
+            </UserProvider>
+        </Insights>
     );
 };
 
