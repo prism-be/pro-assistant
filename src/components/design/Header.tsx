7d4b27da2e6801f8e0f0e9dc7642777ea03694be
Simon Baudart <simon@contact-me.be>
2023-03-15 15:12:32 +0100

    Allow logout :)

--- src/components/design/Header.tsx
+++ src/components/design/Header.tsx
@@ -6,6 +6,7 @@ import useTranslation from "next-translate/useTranslation";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import { toggledMobileMenu } from "@/modules/events/mobileMenu";
 import Link from "next/link";
+import * as process from "process";
 
 const Header = () => {
     const { t } = useTranslation("common");
@@ -21,21 +22,14 @@ const Header = () => {
             <div className={"h-14 flex border-b shadow"}>
                 <div className={"p-1"} onClick={() => toggledMobileMenu()}>
                     <div>
-                        <Image
-                            loader={myLoader}
-                            src="/images/logo.svg"
-                            height={42}
-                            width={42}
-                            alt={"ProAssistant by PRISM"}
-                            unoptimized={true}
-                        ></Image>
+                        <Image loader={myLoader} src="/images/logo.svg" height={42} width={42} alt={"ProAssistant by PRISM"} unoptimized={true}></Image>
                     </div>
                 </div>
                 <div className={"grow"}></div>
                 <div className={"flex"}>
                     <div className="m-auto pl-2 pr-2 text-sm">
                         {t("header.hello")} {user?.name} !<br />
-                        <Link href="/api/auth/logout">{t("header.logout")}</Link>
+                        <a href={`/api/auth/logout?returnUrl=${encodeURIComponent(process.env.NEXT_PUBLIC_BASE_URL ?? "/")}`}>{t("header.logout")}</a>
                     </div>
                 </div>
             </div>
