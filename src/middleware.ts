8644ef017a06be2bfc08a7ae4abaf2960c2dbb34
Simon Baudart <simon@contact-me.be>
2023-03-15 16:38:55 +0100

    Chain middlewares

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,19 @@
 ﻿import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
+import { NextFetchEvent, NextMiddleware, NextRequest } from "next/server";
 
-export default withMiddlewareAuthRequired();
+type MiddlewareFactory = (middleware: NextMiddleware) => NextMiddleware;
+
+const withHeaders: MiddlewareFactory = (next: NextMiddleware) => {
+    return async (request: NextRequest, _next: NextFetchEvent) => {
+        const res = await next(request, _next);
+        if (res) {
+            res.headers.set("x-content-type-options", "nosniff");
+            res.headers.set("x-dns-prefetch-control", "false");
+            res.headers.set("x-download-options", "noopen");
+            res.headers.set("x-frame-options", "SAMEORIGIN");
+        }
+        return res;
+    };
+};
+
+export default withHeaders(withMiddlewareAuthRequired());
