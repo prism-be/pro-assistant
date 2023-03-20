c716b50171c1e79ad863c3796c715a4a9fdb5b17
Simon Baudart <simon@contact-me.be>
2023-03-20 14:05:59 +0100

    Improve coverage

---
+++ api/Prism.ProAssistant.Api.Tests/Extensions/HostBuilderExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file = "HostBuilderExtensionsTests.cs" company = "Prism">
+//  Copyright (c) Prism.All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+using FluentAssertions;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.AspNetCore.Builder;
+using Prism.ProAssistant.Api.Extensions;
+using Prism.ProAssistant.Api.Insights;
+using Serilog;
+using Xunit;
+
+namespace Prism.ProAssistant.Api.Tests.Extensions;
+
+public class HostBuilderExtensionsTests
+{
+
+    [Fact]
+    public void AddApplicationInsights_Ok()
+    {
+        // Arrange
+        var builder = WebApplication.CreateBuilder();
+
+        // Act
+        builder.AddApplicationInsights();
+
+        // Assert
+        builder.Services.Should().Contain(x => x.ServiceType == typeof(ITelemetryInitializer));
+        builder.Services.Should().Contain(x => x.ImplementationType == typeof(RoleNameInitializer));
+    }
+
+    [Fact]
+    public void AddSerilog_Ok()
+    {
+        // Arrange
+        var builder = WebApplication.CreateBuilder();
+
+        // Act
+        builder.AddSerilog();
+
+        // Assert
+        builder.Services.Should().Contain(x => x.ServiceType == typeof(IDiagnosticContext));
+    }
+}
