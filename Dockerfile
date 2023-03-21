47bef6c572e56a37900dae79eaba4b3ae06d6af7
Simon Baudart <simon@contact-me.be>
2023-03-21 15:13:42 +0100

    Create a centralized Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,52 @@
+﻿# Build the web app
+FROM node:18-alpine AS web-deps
+
+WORKDIR /app
+COPY ./web/package.json ./web/yarn.lock ./
+COPY ./web/.yarnrc.docker.yml .yarnrc.yml
+
+RUN corepack enable \
+    && corepack prepare yarn@stable --activate \
+    && yarn set version 3
+    
+RUN yarn install --immutable
+
+FROM node:18-alpine AS web-builder
+
+WORKDIR /app
+
+ENV NODE_ENV=production
+
+COPY ./web .
+
+COPY --from=web-deps /app/.pnp.cjs ./
+COPY --from=web-deps /app/.pnp.loader.mjs ./
+COPY --from=web-deps /app/.yarn ./.yarn
+COPY --from=web-deps /app/.yarnrc.yml ./.yarnrc.yml
+
+RUN corepack enable \
+    && corepack prepare yarn@stable --activate \
+    && yarn set version 3
+    
+RUN yarn install --immutable
+RUN yarn export
+
+# Build the API
+FROM mcr.microsoft.com/dotnet/sdk:7.0 AS net-builder
+
+WORKDIR /source
+
+COPY ./api .
+RUN dotnet restore
+RUN dotnet publish Prism.ProAssistant.Api/Prism.ProAssistant.Api.csproj -c release -o /app --no-restore
+
+# final stage/image
+FROM mcr.microsoft.com/dotnet/aspnet:7.0-jammy
+EXPOSE 80
+ENV DOTNET_RUNNING_IN_CONTAINER=true
+ENV DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=false
+RUN apt-get update && apt-get install -y ttf-mscorefonts-installer fontconfig libc6 libc6-dev libgtk2.0-0 libnss3 libatk-bridge2.0-0 libx11-xcb1 libxcb-dri3-0 libdrm-common libgbm1 libasound2 libappindicator3-1 libxrender1 libfontconfig1 libxshmfence1
+WORKDIR /app
+COPY --from=net-builder /app .
+COPY --from=web-builder /app/out ./wwwroot
+ENTRYPOINT ["dotnet", "Prism.ProAssistant.Api.dll"]
--- api/Prism.ProAssistant.Api/Extensions/WebApplicationExtensions.cs
+++ api/Prism.ProAssistant.Api/Extensions/WebApplicationExtensions.cs
@@ -7,18 +7,19 @@ public static class WebApplicationExtensions
     public static void ReplaceEnvironmentVariables(this WebApplication app)
     {
         app.Logger.LogInformation("Replacing environment variables in JavaScript files...");
+
         foreach (var file in Directory.GetFiles(app.Environment.WebRootPath, "*.js", SearchOption.AllDirectories))
         {
             app.Logger.LogInformation("Replacing environment variables in {file}...", file);
             var content = File.ReadAllText(file);
-            
+
             content = ReplaceEnvironmentVariable(content, app.Logger, "ENV_AZURE_AD_CLIENT_ID", Environment.GetEnvironmentVariable("AZURE_AD_CLIENT_ID"));
             content = ReplaceEnvironmentVariable(content, app.Logger, "ENV_AZURE_AD_TENANT_ID", Environment.GetEnvironmentVariable("AZURE_AD_TENANT_ID"));
             content = ReplaceEnvironmentVariable(content, app.Logger, "ENV_AZURE_AD_USER_FLOW", Environment.GetEnvironmentVariable("AZURE_AD_USER_FLOW"));
             content = ReplaceEnvironmentVariable(content, app.Logger, "ENV_AZURE_AD_TENANT_NAME", Environment.GetEnvironmentVariable("AZURE_AD_TENANT_NAME"));
-            
+
             content = content.Replace("ENV_APPLICATIONINSIGHTS_CONNECTION_STRING", Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING"));
-            
+
             File.WriteAllText(file, content);
         }
     }
@@ -29,14 +30,14 @@ public static class WebApplicationExtensions
         {
             throw new NotFoundException($"The environment variable {variable} was not found.");
         }
-        
+
         var size = content.Length;
         content = content.Replace(variable, value);
 
-        logger.LogInformation(content.Length == size ? 
-            "The environment variable {variable} was not found in the JavaScript file" : 
-            "The environment variable {variable} was replaced in the JavaScript file",
-            variable);
+        if (content.Length != size)
+        {
+            logger.LogInformation("The environment variable {variable} was replaced in the JavaScript file", variable);
+        }
 
         return content;
     }
