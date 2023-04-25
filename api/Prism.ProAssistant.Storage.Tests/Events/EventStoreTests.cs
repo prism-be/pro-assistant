640aad12c9efff28495d536b26d08f7d1a4b1d2b
Simon Baudart <simon@contact-me.be>
2023-04-25 12:18:49 +0200

    Add unit tests

--- api/Prism.ProAssistant.Storage.Tests/Events/EventStoreTests.cs
+++ api/Prism.ProAssistant.Storage.Tests/Events/EventStoreTests.cs
@@ -5,6 +5,8 @@ using Prism.Core;
 using Prism.Infrastructure.Authentication;
 using Prism.Infrastructure.Providers;
 using Prism.ProAssistant.Domain;
+using Prism.ProAssistant.Domain.Configuration.DocumentConfiguration;
+using Prism.ProAssistant.Domain.Configuration.DocumentConfiguration.Events;
 using Prism.ProAssistant.Domain.DayToDay.Contacts;
 using Prism.ProAssistant.Domain.DayToDay.Contacts.Events;
 using Prism.ProAssistant.Storage.Events;
@@ -78,5 +80,44 @@ public class EventStoreTests
         // Assert
         eventContainer.Verify(x => x.WriteAsync(It.IsAny<string>(), It.IsAny<DomainEvent>()), Times.Once);
         container.Verify(x => x.WriteAsync(It.IsAny<string>(), It.IsAny<Contact>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task RaiseAndPersist_Ok_Delete()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<EventStore>>();
+        var userOrganization = new UserOrganization();
+        var streamId = Identifier.GenerateString();
+
+        var eventContainer = new Mock<IStateContainer<DomainEvent>>();
+        var container = new Mock<IStateContainer<DocumentConfiguration>>();
+        var stateProvider = new Mock<IStateProvider>();
+        var @event = new DocumentConfigurationDeleted
+        {
+            StreamId = streamId
+        };
+        eventContainer.Setup(x => x.FetchAsync(It.IsAny<Filter[]>())).ReturnsAsync(new List<DomainEvent>
+        {
+            new()
+            {
+                Data = JsonSerializer.Serialize(@event),
+                Type = nameof(DocumentConfigurationDeleted),
+                Id = Identifier.GenerateString(),
+                StreamId = streamId,
+                StreamType = "documents-configuration",
+                UserId = Identifier.GenerateString()
+            }
+        });
+        stateProvider.Setup(x => x.GetContainerAsync<DocumentConfiguration>()).ReturnsAsync(container.Object);
+        stateProvider.Setup(x => x.GetContainerAsync<DomainEvent>()).ReturnsAsync(eventContainer.Object);
+
+        // Act
+        var eventStore = new EventStore(logger.Object, stateProvider.Object, userOrganization);
+        await eventStore.RaiseAndPersist<DocumentConfiguration>(@event);
+
+        // Assert
+        eventContainer.Verify(x => x.WriteAsync(It.IsAny<string>(), It.IsAny<DomainEvent>()), Times.Once);
+        container.Verify(x => x.DeleteAsync(It.IsAny<string>()), Times.Once);
     }
 }
