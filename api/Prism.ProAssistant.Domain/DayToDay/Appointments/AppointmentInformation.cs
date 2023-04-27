e35fce825bff2bd23022a1bf78b56a8ea02144e6
Simon Baudart <simon@contact-me.be>
2023-04-27 14:13:09 +0200

    Minimize the appointment informations in event

---
+++ api/Prism.ProAssistant.Domain/DayToDay/Appointments/AppointmentInformation.cs
@@ -0,0 +1,51 @@
+﻿namespace Prism.ProAssistant.Domain.DayToDay.Appointments;
+
+using System.Text.Json.Serialization;
+
+public class AppointmentInformation
+{
+    [JsonPropertyName("birthDate")]
+    public string? BirthDate { get; set; }
+
+    [JsonPropertyName("contactId")]
+    public string? ContactId { get; set; }
+
+    [JsonPropertyName("duration")]
+    public int Duration { get; set; }
+
+    [JsonPropertyName("firstName")]
+    public string? FirstName { get; set; }
+
+    [JsonPropertyName("id")]
+    public required string Id { get; set; }
+
+    [JsonPropertyName("lastName")]
+    public string? LastName { get; set; }
+
+    [JsonPropertyName("payment")]
+    public int Payment { get; set; }
+
+    [JsonPropertyName("paymentDate")]
+    public DateTime? PaymentDate { get; set; }
+
+    [JsonPropertyName("phoneNumber")]
+    public string? PhoneNumber { get; set; }
+
+    [JsonPropertyName("price")]
+    public decimal Price { get; set; }
+
+    [JsonPropertyName("startDate")]
+    public DateTime StartDate { get; set; }
+
+    [JsonPropertyName("state")]
+    public int State { get; set; }
+
+    [JsonPropertyName("title")]
+    public string? Title { get; set; }
+
+    [JsonPropertyName("type")]
+    public string? Type { get; set; }
+
+    [JsonPropertyName("typeId")]
+    public string? TypeId { get; set; }
+}
---
+++ api/Prism.ProAssistant.Domain/DayToDay/Appointments/AppointmentInformationMapper.cs
@@ -0,0 +1,10 @@
+﻿namespace Prism.ProAssistant.Domain.DayToDay.Appointments;
+
+using Riok.Mapperly.Abstractions;
+
+[Mapper]
+public static partial class AppointmentInformationMapper
+{
+    public static partial Appointment ToAppointment(this AppointmentInformation appointmentInformation);
+    public static partial void ToAppointment(this AppointmentInformation appointmentInformation, Appointment appointment);
+}
